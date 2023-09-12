f7236c61569265b79c23d70dd25b930fe1b9d72e
ezafackkenfack <ezafackkenfack@sedona.fr>
2023-09-12 16:59:11 +0200

    creating of all dtos and mapper for each dtos classes

---
+++ src/main/java/com/java/bankingaccount/dto/TransactionDto.java
@@ -0,0 +1,46 @@
+package com.java.bankingaccount.dto;
+
+import com.java.bankingaccount.models.Transaction;
+import com.java.bankingaccount.models.User;
+import com.java.bankingaccount.utils.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * DTO for {@link com.java.bankingaccount.models.Transaction}
+ */
+@Data
+@AllArgsConstructor
+@Builder
+public class TransactionDto implements Serializable {
+    private Integer id;
+    private BigDecimal amount;
+    private TransactionType type;
+    private String destinationIban;
+    private Integer userId;
+
+    public static TransactionDto fromTransaction(Transaction transaction) {
+        return TransactionDto.builder()
+                .id(transaction.getId())
+                .amount(transaction.getAmount())
+                .type(transaction.getType())
+                .destinationIban(transaction.getDestinationIban())
+                .userId(transaction.getUser().getId())
+                .build();
+    }
+
+    public static Transaction toTransactionDto(TransactionDto transaction) {
+        return Transaction.builder()
+                .id(transaction.getId())
+                .amount(transaction.getAmount())
+                .type(transaction.getType())
+                .destinationIban(transaction.getDestinationIban())
+                .user(User.builder().id(transaction.getUserId()).build())
+                .build();
+    }
+}
